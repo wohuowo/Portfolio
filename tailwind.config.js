6245ce30d8348b3e9315dd36dfa1c8828aecd933
Tony Obriku <tonyobriku@gmail.com>
2022-04-29 01:41:03 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   content: [
-    './components/**/*.{html,js}',
-    './pages/**/*.{html,js}',
-    './index.html',
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./index.html",
   ],
   theme: {
     extend: {
       colors: {
-        "body": '#17171F',
-        'selected-text': '#A3A3FF',
-        'theme': '#3F3FFF',
-        'nav': '#404053',
-        'secondary': '#9191A4',
-        'badge': '#3F3F51',
-        'input-border': '#565666',
-        'input': '#2A2A35'
+        body: "black",
+        "selected-text": "#A3A3FF",
+        theme: "#3F3FFF",
+        nav: "#404053",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
+        "input-border": "#565666",
+        input: "#2A2A35",
       },
       fontFamily: {
-        'poppins': ["'Poppins'", 'san-serif']
-      }
+        poppins: ["'Poppins'", "san-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
